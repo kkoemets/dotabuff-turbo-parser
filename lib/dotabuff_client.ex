e9bdc3a9a634fe64d2cf4a582147d9ed50d88511
kkoemets <koemets.kristjan@gmail.com>
2023-08-19 18:34:22 +0300

    Add WIP row parsing

--- lib/dotabuff_client.ex
+++ lib/dotabuff_client.ex
@@ -1,30 +1,59 @@
 defmodule DotabuffClient do
-require Logger
+  require Logger
 
   @doc false
   def get_html() do
-    Logger.info "Getting HTML"
+    Logger.info("Getting HTML")
+
     case HTTPoison.get("https://www.dotabuff.com/matches?game_mode=turbo") do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
         {:ok, body}
+
       {:ok, %HTTPoison.Response{status_code: status_code}} ->
         {:error, "Received status code #{status_code}"}
+
       {:error, %HTTPoison.Error{reason: reason}} ->
         {:error, reason}
     end
   end
 
- @doc false
+  @doc false
   def get_table(html) do
-    Logger.info "Getting table"
+    Logger.info("Getting table")
     {:ok, document} = Floki.parse_document(html)
     Floki.raw_html(Floki.find(document, "table"))
   end
 
   @doc false
   def get_rows(html_table) do
-    Logger.info "Finding rows"
+    Logger.info("Finding rows")
     {:ok, document} = Floki.parse_document(html_table)
     Floki.raw_html(Floki.find(document, "tr"))
+  end
+
+  @doc false
+  def create_data(html_rows) do
+    Logger.info("Creating data")
+    {:ok, document} = Floki.parse_document(html_rows)
+    rows = remove_first_element(Floki.find(document, "tr"))
+    row = Enum.at(rows, 0)
+    parse_row(row)
+  end
+
+  @doc false
+  def parse_row(map_row) do
+    Logger.info("Parsing row")
+    columns = Floki.find(map_row, "td")
+    parse_column(Enum.at(columns, 0))
+  end
+
+  @doc false
+  def parse_column(column) do
+    Logger.info("Parsing column")
+    Logger.info(inspect(column))
+  end
+
+  defp remove_first_element(list) do
+    List.delete(list, Enum.at(list, 0))
   end
 end
--- test/dotabuff_client_test.exs
+++ test/dotabuff_client_test.exs
@@ -4,17 +4,45 @@ defmodule DotabuffClientTest do
 
   test "gets html" do
     {:ok, html} = DotabuffClient.get_html()
-    assert String.contains? html, "Turbo"
+    assert String.contains?(html, "Turbo")
   end
 
   test "gets table" do
-    {:ok, html} = DotabuffClient.get_html()
-    assert String.contains? DotabuffClient.get_table(html), "<table><thead><tr><th>Match ID</th><th>Game Mode</th><th>"
+    {:ok, html} = get_test_html()
+
+    assert String.contains?(
+             DotabuffClient.get_table(html),
+             "<table><thead><tr><th>Match ID</th><th>Game Mode</th><th>"
+           )
   end
 
   test "gets rows" do
-    {:ok, html} = DotabuffClient.get_html()
-    assert String.contains? DotabuffClient.get_rows(DotabuffClient.get_table(html)), ">Dire Victory</a>"
+    {:ok, html} = get_test_html()
+
+    assert String.contains?(
+             DotabuffClient.get_rows(DotabuffClient.get_table(html)),
+             "th>Match ID</th>"
+           )
   end
 
+  test "create data" do
+    {:ok, html_rows} = get_test_rows()
+    DotabuffClient.create_data(html_rows)
+  end
+
+  def get_test_html() do
+    {:ok, file_content} = File.read("test/turbo_sample_page.html")
+    html = remove_linebreaks(IO.chardata_to_string(file_content))
+    {:ok, html}
+  end
+
+  def get_test_rows() do
+    {:ok, file_content} = File.read("test/sample_rows.html")
+    html_rows = remove_linebreaks(IO.chardata_to_string(file_content))
+    {:ok, html_rows}
+  end
+
+  def remove_linebreaks(html) do
+    String.replace(html, ~r/\n/, "")
+  end
 end
