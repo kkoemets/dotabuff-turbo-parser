323d9959927c88f183ac4380c2b429047659a52a
kkoemets <koemets.kristjan@gmail.com>
2023-08-20 22:21:47 +0300

    Add parsing for some match fields

--- lib/dotabuff_client.ex
+++ lib/dotabuff_client.ex
@@ -43,17 +43,60 @@ defmodule DotabuffClient do
   @doc false
   def parse_row(map_row) do
     Logger.info("Parsing row")
-    columns = Floki.find(map_row, "td")
-    parse_column(Enum.at(columns, 0))
+    columns = get_columns(map_row)
+    parse_datetime(columns)
   end
 
   @doc false
-  def parse_column(column) do
-    Logger.info("Parsing column")
-    Logger.info(inspect(column))
+  def get_columns(map_row) do
+    Logger.info("Finding columns")
+    Floki.find(map_row, "td")
   end
 
-  defp remove_first_element(list) do
+  @doc false
+  def parse_datetime(columns) do
+    Logger.info("Parsing datetime")
+    time_element = Floki.find(Enum.at(columns, 0), "time")
+
+    Enum.at(Floki.attribute(time_element, "datetime"), 0)
+  end
+
+  @doc false
+  def parse_result(columns) do
+    Logger.info("Parsing result")
+    result_element = Floki.find(Enum.at(columns, 2), "a")
+
+    Floki.text(result_element)
+  end
+
+  @doc false
+  def parse_radiant_team(columns) do
+    Logger.info("Parsing radiant team")
+    radiant_team_elements = Floki.find(Enum.at(columns, 4), "div a")
+
+    Enum.map(radiant_team_elements, fn element ->
+      case Floki.attribute(element, "href") do
+        [href | _] -> List.last(String.split(href, "/"))
+        [] -> nil
+      end
+    end)
+  end
+
+  @doc false
+  def parse_dire_team(columns) do
+    Logger.info("Parsing dire team")
+    dire_team_elements = Floki.find(Enum.at(columns, 5), "div a")
+
+    Enum.map(dire_team_elements, fn element ->
+      case Floki.attribute(element, "href") do
+        [href | _] -> List.last(String.split(href, "/"))
+        [] -> nil
+      end
+    end)
+  end
+
+  @doc false
+  def remove_first_element(list) do
     List.delete(list, Enum.at(list, 0))
   end
 end
--- test/dotabuff_client_test.exs
+++ test/dotabuff_client_test.exs
@@ -30,6 +30,48 @@ defmodule DotabuffClientTest do
     DotabuffClient.create_data(html_rows)
   end
 
+  test "parse datetime" do
+    row = get_test_row()
+
+    assert String.contains?(
+             DotabuffClient.parse_datetime(DotabuffClient.get_columns(row)),
+             "2023-08-19T14:20:09+00:00"
+           )
+  end
+
+  test "parse result" do
+    row = get_test_row()
+
+    assert String.contains?(
+             DotabuffClient.parse_result(DotabuffClient.get_columns(row)),
+             "Radiant Victory"
+           )
+  end
+
+  test "parse radiant team" do
+    row = get_test_row()
+
+    assert DotabuffClient.parse_radiant_team(DotabuffClient.get_columns(row)) == [
+             "phantom-assassin",
+             "bloodseeker",
+             "mirana",
+             "dark-seer",
+             "clinkz"
+           ]
+  end
+
+  test "parse dire team" do
+    row = get_test_row()
+
+    assert DotabuffClient.parse_dire_team(DotabuffClient.get_columns(row)) == [
+             "meepo",
+             "lina",
+             "morphling",
+             "legion-commander",
+             "arc-warden"
+           ]
+  end
+
   def get_test_html() do
     {:ok, file_content} = File.read("test/turbo_sample_page.html")
     html = remove_linebreaks(IO.chardata_to_string(file_content))
@@ -40,6 +82,13 @@ defmodule DotabuffClientTest do
     {:ok, file_content} = File.read("test/sample_rows.html")
     html_rows = remove_linebreaks(IO.chardata_to_string(file_content))
     {:ok, html_rows}
+  end
+
+  def get_test_row() do
+    {:ok, html_rows} = get_test_rows()
+    {:ok, document} = Floki.parse_document(html_rows)
+    rows = DotabuffClient.remove_first_element(Floki.find(document, "tr"))
+    Enum.at(rows, 0)
   end
 
   def remove_linebreaks(html) do
